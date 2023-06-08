7ba04e3d47f41a0f819138e3955d9d9bdceff304
Mac <maac@realme-5-Pro.localdomain>
2023-06-08 15:48:49 +0200

    fix: fix

--- api/washer.ts
+++ api/washer.ts
@@ -1,6 +1,31 @@
 // const express = require("express");
 import { Router } from 'express';
 const Washer = require("../models/washerModel");
+var fs = require("fs");
+var multer = require("multer");
+var date_now = Date.now();
+var storage = multer.diskStorage({
+  destination: function (req: any, file: any, cb: any) {
+    cb(null, "../Front-end/auto-perionica/src/assets/");
+  },
+  filename: function (req: any, file: any, cb: any) {
+    cb(null, date_now + file.originalname);
+  },
+});
+
+//UPLOAD IAMGE
+
+var fileFilter = (req: any, file: any, cb: any) => {
+  //reject a file
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+
+var upload = multer({ storage: storage, fileFilter: fileFilter });
 
 var router = Router();
 
@@ -23,8 +48,8 @@ router.get("/:id", (req: any, res: any) => {
       console.log(`${err} : GetOneWasher`);
     });
 });
-
-router.post("/", (req: any, res: any) => {
+// UPLAOAD IMAGE
+router.post("/",(req: any, res: any) => {
   console.log('dusan', req.body);
   const client = new Washer({
     title: req.body.title,
@@ -33,6 +58,11 @@ router.post("/", (req: any, res: any) => {
     service: req.body.service,
     userRef: req.body.userRef,
     session: req.body.session,
+    contact: req.body.contact,
+    imagePath: req.body.imagePath,
+    review: req.body.review,
+    workingHours: req.body.workingHours,
+    workingDays: req.body.workingDays,
   });
   client.save().then((result:any)=> {
     res.send(result);
@@ -48,9 +78,14 @@ router.put("/:id", async (req: any, res: any) => {
       result.title = req.body.title;
       result.adress = req.body.adress;
       result.coordinate = req.body.coordinate;
-      result.service = req.body.service;
-      result.userRef = req.body.userRef;
-      result.session = req.body.session;
+      result.service = req.body.service;//fix
+      result.userRef = req.body.userRef;//fix
+      result.session = req.body.session;//fix
+      result.contact = req.body.contact;
+      result.review = req.body.review;
+      result.imagePath = req.body.imagePath,
+      result.workingHours = req.body.workingHours,
+      result.workingDays = req.body.workingDays,
       await result.save();
       res.send('success')
     })
